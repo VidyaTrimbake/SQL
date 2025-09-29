9df05f310438194c688468bf657f31d631565310
Vidya Trimbake <vidyatrimbake3@gmail.com>
2025-09-29 20:44:50 +0530

    Delete dev.sql

--- dev.sql
+++
@@ -1,6 +0,0 @@
-create table rama( first_name VARCHAR(12),middle_name Varchar (12),last_name VARCHAR(12));
-alter table rama add last_name VARCHAR(12);
-desc rama;
-select *from rama;
-select first_name from rama;
-rename rama to servey
