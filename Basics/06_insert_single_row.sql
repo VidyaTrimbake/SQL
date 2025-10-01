23085f16a8d6595de90a4b089f961ef9dff4e93d
Vidya Trimbake <vidyatrimbake3@gmail.com>
2025-10-01 17:08:15 +0530

    Create 06_insert_single_row.sql
    Basic INSERT syntax; mapping columns to values.
    This statement uses the INSERT INTO command to add a new row of data into the Employees table. It explicitly lists the columns to be populated (EmployeeID, FirstName, LastName, Salary) and provides the corresponding VALUES for those columns in the correct order.

---
+++ Basics/06_insert_single_row.sql
@@ -0,0 +1,13 @@
+Que.
+Insert a new employee, 'Frank White' (ID 105, Salary 70000) into the Employees table.	
+  
+Solution:
+INSERT INTO Employees (
+  EmployeeID, 
+  FirstName, 
+  LastName, 
+  Salary) 
+VALUES (105, 
+  'Frank', 
+  'White', 
+  70000.00);
