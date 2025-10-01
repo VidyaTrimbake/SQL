186e1174a8eb8f4d06702162827cabc33d99e255
Vidya Trimbake <vidyatrimbake3@gmail.com>
2025-10-01 15:40:46 +0530

    Create 01_create_table.sql
    DDL, Schemas, PRIMARY KEY (unique, non-null ID).

---
+++ Basics/01_create_table.sql
@@ -0,0 +1,7 @@
+Que. 
+How do you create a table named Products with ID (Primary Key) and Name (Cannot be Null)?
+
+Solution:
+CREATE TABLE Products 
+(ProductID INT PRIMARY KEY,   
+Name VARCHAR(100) NOT NULL);
