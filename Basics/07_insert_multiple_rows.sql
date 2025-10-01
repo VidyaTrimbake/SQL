de5d9b3839ad05b9eca6a2427214184992c328db
Vidya Trimbake <vidyatrimbake3@gmail.com>
2025-10-01 17:11:41 +0530

    Create 07_insert_multiple_rows.sql
    This technique, known as a multi-row insert, allows you to add multiple records efficiently with one INSERT INTO statement. After specifying the table and column names once, you simply separate each new row of VALUES with a comma inside the parentheses.

---
+++ Basics/07_insert_multiple_rows.sql
@@ -0,0 +1,12 @@
+Que.
+Insert two new employees in a single statement.	
+  
+Solution:
+INSERT INTO Employees 
+  (EmployeeID, 
+  FirstName, 
+  LastName, 
+  Salary)
+VALUES
+    (106, 'Jane', 'Doe', 65000.00),
+    (107, 'John', 'Smith', 75000.00);
